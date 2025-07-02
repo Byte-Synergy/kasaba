f986885aa17888cc9f256f4dd5f5f8d1b4939ada
Ibragim <ibragim@MacBook-Air-Ibragim.local>
2025-07-02 21:40:45 +0500

    add meta tags to admin panel

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -26,10 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         {/* Telegram WebApp SDK */}
         <Script src="/telegram-web-app.js" strategy="beforeInteractive" />
-      </head>
+      </Head>
       <body
         className={cn(
           "min-h-screen overflow-hidden bg-slate-100 antialiased dark:bg-slate-900",

de74e18384107023fc3b9d16e117046bac2c3e8c
Byte Synergy <bytesynergyofficial@gmail.com>
2025-07-21 14:46:10 +0500

    fixed telegram bot

--- davadmin__bot/src/core/bot.js
+++ davadmin__bot/src/core/bot.js
@@ -5,8 +5,8 @@ config();
 const { BOT__TOKEN } = process.env;
 
 if (!BOT__TOKEN) {
-    throw new Error("BOT_TOKEN aniqlanmadi. Iltimos .env faylni tekshiring.");
+  throw new Error("BOT_TOKEN aniqlanmadi. Iltimos .env faylni tekshiring.");
 }
 const bot = new Telegraf(BOT__TOKEN || "");
 
-module.exports = bot
+module.exports = bot;
--- davadmin__bot/src/services/login.service.js
+++ davadmin__bot/src/services/login.service.js
@@ -2,24 +2,27 @@ const axios = require("axios");
 const { config } = require("dotenv");
 config();
 
-
 async function Login(username, password) {
   try {
-    if (!process.env.API__URL) {
+    if (!process.env.NEXT_PUBLIC_API_URL) {
       throw new Error("API_URL is not defined in the environment variables");
     }
 
-    const response = await axios.post(`${process.env.API__URL}/api/rest/auth/signIn`, {
-      username,
-      password,
-    }, {
-      headers: {
-        "Content-Type": "application/json",
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/rest/auth/signIn`,
+      {
+        username,
+        password,
       },
-    });
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-    const data = response.data
-    
+    const data = response.data;
+
     if (Object.keys(data).length) {
       return {
         success: true,
@@ -31,7 +34,6 @@ async function Login(username, password) {
         error: "Логин ёки парол нотўғри.",
       };
     }
-
   } catch (error) {
     console.error("Auth error:", error?.response?.data || error.message);
 

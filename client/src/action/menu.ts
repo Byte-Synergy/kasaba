1990f92474313e6e99081cced536d18f8be012da
dr Coder <drcoderjs@gmail.com>
2025-08-26 22:17:27 +0500

    fixed bug in deployment proccess Update menu.ts

--- client/src/action/menu.ts
+++ client/src/action/menu.ts
@@ -1,27 +1,53 @@
+"use server";
+
 import eden from "@/libs/eden";
 import { AppType } from "@/types/server";
 
-export async function getMenus(
-  query: AppType["~Routes"]["api"]["rest"]["menus"]["get"]["query"],
-  fetch?: Omit<RequestInit, "headers" | "method">,
-  fetcher: typeof eden = eden
-) {
-  const { data, error, status } = await fetcher.menus.get({
-    fetch,
-    query,
+export type MenuItem = {
+  id: string;
+  title: string;
+  path: string;
+  docCount?: number;
+  sub_menu: MenuItem[];
+  type?: AppType["~Routes"]["api"]["rest"]["menus"]["tree"]["get"]["response"]["200"][number]["type"];
+  newsType?: AppType["~Routes"]["api"]["rest"]["menus"]["tree"]["get"]["response"]["200"][number]["newsType"];
+};
+
+// Eden’dan menu olish
+export async function getMenuTree(lang: string) {
+  const { data, error, status } = await eden.menus.tree.get({
+    query: {
+      page: 1,
+      filter: {
+        lang,
+        orderBy: "asc",
+        parentId: null,
+      },
+    },
   });
 
-  return { data, error, status };
-}
+  if (error) {
+    return { success: false, error, status, data: [] };
+  }
 
-export async function getMenu(
-  menuId: number,
-  fetch?: Omit<RequestInit, "headers" | "method">,
-  fetcher: typeof eden = eden
-) {
-  const { data, error, status } = await fetcher
-    .menus({ menuId })
-    .get({ fetch });
+  // Menuni mapping qilish
+  function mapMenuTree(
+    menu: AppType["~Routes"]["api"]["rest"]["menus"]["tree"]["get"]["response"]["200"][number]
+  ): MenuItem {
+    return {
+      type: menu.type,
+      id: menu.id.toString(),
+      title: menu.name,
+      path: menu.path || "",
+      newsType: menu.newsType,
+      docCount: menu.type === "document" ? menu.files?.length : undefined,
+      sub_menu: (menu.children || []).map(mapMenuTree),
+    };
+  }
 
-  return { data, error, status };
+  return {
+    success: true,
+    status,
+    data: (data || []).map(mapMenuTree),
+  };
 }

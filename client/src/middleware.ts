95bd6b90bc7c6286082c75a3505a738863623484
dr Coder <drcoderjs@gmail.com>
2026-03-27 19:29:58 +0500

    Update middleware.ts

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -21,16 +21,11 @@ function getLocale(request: NextRequest): string | undefined {
 export default function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
-  // // If you have one
-  // if (
-  //   [
-  //     "/manifest.json",
-  //     "/favicon.ico",
-  //     // Your other files in `public`
-  //   ].includes(pathname)
-  // )
-  //   return;
+  const isMaintenance = process.env.NEXT_PUBLIC_MAINTENANCE === "true";
+
+  if (isMaintenance && req.nextUrl.pathname !== "/maintenance") {
+    return NextResponse.redirect(new URL("/maintenance", req.url));
+  }
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
